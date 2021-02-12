a87a5f4a2d97359888da32ccc05f38795d23ebea
chandrikarao29 <78976924+chandrikarao29@users.noreply.github.com>
2021-02-12 15:59:29 -0500

    Update HelloWorldApplication.java

--- src/main/java/com/example/demo/HelloWorldApplication.java
+++ src/main/java/com/example/demo/HelloWorldApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class HelloWorldApplication {
-
+//add comment
 	public static void main(String[] args) {
 		SpringApplication.run(HelloWorldApplication.class, args);
 	}
